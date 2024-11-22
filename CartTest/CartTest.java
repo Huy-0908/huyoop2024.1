ff859d92f407201aecbaf793e7fd628075c7d914
Huy-0908 <Huy.hq235594@sis.hust.edu.vn>
2024-11-22 18:48:53 +0700

    Update CartTest.java

--- CartTest/CartTest.java
+++ CartTest/CartTest.java
@@ -4,11 +4,11 @@ import hust.soict.dsai.aims.cart.Cart.Cart;
 public class CartTest {
 	public static void main(String[] args) {
 		Cart cart = new Cart();
-		DigitalVideoDiscs dvd1 = new DigitalVideoDiscs("The Lion King","Animation","Roger Allers",87,19.95f);
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation","Roger Allers",87,19.95f);
 		cart.addDigitalVideoDisc(dvd1);
-		DigitalVideoDiscs dvd2 = new DigitalVideoDiscs("Star Wars","Science Fiction","Geogre Lucas",87,24.95f);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars","Science Fiction","Geogre Lucas",87,24.95f);
 		cart.addDigitalVideoDisc(dvd2);
-		DigitalVideoDiscs dvd3 = new DigitalVideoDiscs("Aladin","Animation","Roger Calks",87,18.99f);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation","Roger Calks",87,18.99f);
 		cart.addDigitalVideoDisc(dvd3);
 		cart.printCartDetails();
 	    cart.searchById(1);
